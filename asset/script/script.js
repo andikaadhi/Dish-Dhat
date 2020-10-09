b1090778e8d2926b94680819698a3b70d89e8cf9
Andika Adhinata <aa80004@student.uph.edu>
2020-10-09 17:39:24 +0700

    fix near me, add menu

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,14 +1,16 @@
 
 function hideNearMe() {
 	var wh = window.innerHeight;
-	mt = wh - 50; 
+	mt = wh - 60; 
 	$(".nearme-container").css({"top" : mt});
-	$(".nearme-container h5").attr("onclick","showNearMe()");
+	$(".nearme-container #near-me").attr("onclick","showNearMe()");
+	$(".nearme-container #near-me img").attr("src","asset/img/arrow-up.png");
 }
 
 function showNearMe() {
 	$(".nearme-container").css({"top" : 64});
-	$(".nearme-container h5").attr("onclick","hideNearMe()");
+	$(".nearme-container #near-me").attr("onclick","hideNearMe()");
+	$(".nearme-container #near-me img").attr("src","asset/img/arrow-down.png");
 }
 
 window.onload=function()
